2e174a5e0ff507c48aae8f866664154bf5a542a2
yeonoel <yeopevroguinoel@gmail.com>
2022-06-16 16:22:14 +0000

    push function

--- monty.h
+++ monty.h
@@ -46,5 +46,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
 
 #endif /* MONTY_H */
